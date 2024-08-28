bdb7122d102c7c6373ccef0e08fa55f1232e2b0b
Yaseen Saad <yaseensaadeldinmohamed@gmail.com>
2024-08-28 19:37:13 +0300

    Finishing Score Board

--- api/index.js
+++ api/index.js
@@ -24,7 +24,7 @@ const app = express();
 // });
 app.post(
   "/upload",
-  upload.single("file"),
+  // upload.single("file"),
   (req, res) => {
     const tempPath = req.file.path;
     const teamId = req.body.teamid;
