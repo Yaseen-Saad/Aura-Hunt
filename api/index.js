176c520d6cc8ff67af8a88317f1f92ad1df8a746
Yaseen Saad Eldin <yaseensaadeldinmohamed@gmail.com>
2024-08-30 14:26:24 +0300

    Setting the advanced score board

--- api/index.js
+++ api/index.js
@@ -29,6 +29,28 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
+// Pages
+// Client
+// Scoreboard
+app.get('/scoreboard', (req, res) => {
+  const scores = scoresFile.map(team => { return { id: team.id, score: team.score } });
+  res.render('scoreboard', { scores: scores });
+})
+// login
+app.post('/teamlogin', (req, res) => {
+  const team = scoresFile.find(team => team.id === req.body.team);
+  if (team) {
+    if (team.password === req.body.password) {
+      res.status(200).send({ message: 'Team logged in successfully', token: team.token });
+    } else {
+      res.status(200).send({ message: 'Wrong password' });
+    }
+  } else {
+    res.status(404).send({ message: 'Team not found' });
+  }
+})
+
+// Image "/upload" endpoint
 const upload = multer({ dest: 'uploads/' });
 app.post('/upload', upload.single('file'), (req, res) => {
   const teamId = req.body.teamid;
@@ -66,61 +88,21 @@ function handleError(err, res) {
   console.error(err);
   res.status(500).send({ message: 'Server Error' });
 }
+
+// All Games End point
 app.get('/games', (req, res) => {
   const games = jsonFile.map(game => `<li><a href="/game/${game.name}">${game.name}</a></li>`);
   res.send(`<!DOCTYPE html><html><body><h1>Game List</h1><ul>${games.join('')}</ul></body></html>`);
 });
+
+// Detaild Scoreboard End point
 app.get('/adminEditScore', (req, res) => {
   // const scores = scoresFile.find(team => +team.id == req.params.id);
   res.render("detailedScoreboard", { scores: scoresFile })
   // res.send({ message: "not found", data: req.params.id })
 });
-// Start the Server
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Server started on port ${port}`);
-});
 
-
-// Route to Get Specific Game's Question by ID
-app.get('/question/:id', (req, res) => {
-  const gameId = parseInt(req.params.id);
-  try {
-    const game = jsonFile.find(game => game.id === gameId);
-    if (!game) {
-      res.status(404).send('Game not found');
-    } else {
-      res.send(game);
-    }
-  } catch (error) {
-    console.error('Error reading JSON file:', error);
-    res.status(500).send('Error reading JSON file');
-  }
-});
 // Route to Submit an Answer
-
-app.get('/score/:id', (req, res) => {
-  const scores = scoresFile.find(team => +team.id == req.params.id);
-  res.render("team", { score: scores })
-});
-app.post('/editscore', (req, res) => {
-  const team = scoresFile.find(team => +team.id == req.body.id);
-  if (req.body.token === process.env.ADMIN_REQUEST_TOKEN) {
-    console.log(team);
-    team.score += Number(req.body.aura);
-    fs.writeFile('./data/scores.json', JSON.stringify(scoresFile, null, 2), err => {
-      if (err) {
-        console.error('Error writing to scores.json:', err);
-        res.status(500).json({ message: 'Failed to update scores' });
-      } else {
-        res.status(200).json({ message: "score added succefully" });
-      }
-    });
-  } else {
-    res.status(500).send("Wrong Token")
-  }
-});
-
 app.post('/answer', (req, res) => {
   const { teamId, gameId, answer } = req.body;
   try {
@@ -161,9 +143,105 @@ app.post('/answer', (req, res) => {
 });
 
 
+
+// Route to toggle checked
+app.post('/toggleChecked', (req, res) => {
+  const { id, questionId } = req.body;
+  try {
+    const team = scoresFile.find(team => team.id === id);
+    if (!team) {
+      return res.status(404).send('Team not found');
+    }
+    const question = team.questions.find(question => question.id == questionId);
+   console.log(id, questionId);
+   
+    if (!question || question.solved) {
+      return res.status(200).json({ message: 'Already Solved' });
+    }
+    question.checked = true;
+    updateScores(scoresFile, res, 'CHECKED');
+  } catch (error) {
+    console.error('Error processing answer:', error);
+    res.status(500).send('Internal server error');
+  }
+});
+// Route to give bonus for answers
+app.post('/correctQuestion', (req, res) => {
+  const { teamId, gameId, correct } = req.body;
+  try {
+    const game = jsonFile.find(game => game.id === parseInt(gameId));
+    if (!game) {
+      return res.status(404).send('Game not found');
+    }
+    const team = scoresFile.find(team => team.id === teamId);
+    if (!team) {
+      return res.status(404).send('Team not found');
+    }
+    const question = team.questions.find(question => question.id === gameId);
+    if (!question || question.solved) {
+      return res.status(200).json({ message: 'Already Solved' });
+    }
+    question.checked = true;
+    if (correct) {
+
+      question.solved = correct;
+      team.score += game.score
+    } else {
+      team.score -= game.deduction
+    }
+    updateScores(scoresFile, res, 'CHECKED');
+  } catch (error) {
+    console.error('Error processing answer:', error);
+    res.status(500).send('Internal server error');
+  }
+});
+
+
+
+// // Route to Get Specific Game's Question by ID
+// app.get('/question/:id', (req, res) => {
+//   const gameId = parseInt(req.params.id);
+//   try {
+//     const game = jsonFile.find(game => game.id === gameId);
+//     if (!game) {
+//       res.status(404).send('Game not found');
+//     } else {
+//       res.send(game);
+//     }
+//   } catch (error) {
+//     console.error('Error reading JSON file:', error);
+//     res.status(500).send('Error reading JSON file');
+// }
+// });
+
+app.get('/score/:id', (req, res) => {
+  const scores = scoresFile.find(team => +team.id == req.params.id);
+  console.log();
+  
+  res.render("team", { score: scores })
+});
+app.post('/editscore', (req, res) => {
+  const team = scoresFile.find(team => +team.id == req.body.id);
+  if (req.body.token === process.env.ADMIN_REQUEST_TOKEN) {
+    console.log(team);
+    team.score += Number(req.body.aura);
+    fs.writeFile('./data/scores.json', JSON.stringify(scoresFile, null, 2), err => {
+      if (err) {
+        console.error('Error writing to scores.json:', err);
+        res.status(500).json({ message: 'Failed to update scores' });
+      } else {
+        res.status(200).json({ message: "score added succefully" });
+      }
+    });
+  } else {
+    res.status(500).send("Wrong Token")
+  }
+});
+
+
 // Helper function to update scores
 const updateScores = (scoresFile, res, message, status = 200, extra = {}) => {
-  fs.writeFile('../data/scores.json', JSON.stringify(scoresFile, null, 2), err => {
+  fs.writeFile('./data/scores.json', JSON.stringify(scoresFile, null, 2), err => {
     if (err) {
       console.error('Error writing to scores.json:', err);
       res.status(500).json({ message: 'Failed to update scores' });
@@ -184,22 +262,7 @@ app.get('/game/:id', (req, res) => {
     res.status(404).send('Not found');
   }
 });
-app.get('/scoreboard', (req, res) => {
-  const scores = scoresFile.map(team => { return { id: team.id, score: team.score } });
-  res.render('scoreboard', { scores: scores });
-})
-app.post('/teamlogin', (req, res) => {
-  const team = scoresFile.find(team => team.id === req.body.team);
-  if (team) {
-    if (team.password === req.body.password) {
-      res.status(200).send({ message: 'Team logged in successfully', token: team.token });
-    } else {
-      res.status(200).send({ message: 'Wrong password' });
-    }
-  } else {
-    res.status(404).send({ message: 'Team not found' });
-  }
-})
+
 app.get('/instructions', (req, res) => {
   const scores = scoresFile.map(team => { return { id: team.id, score: team.score } });
   res.render('instructions', { scores: scores });
@@ -208,6 +271,12 @@ app.get('/instructions', (req, res) => {
 // Catch-all route for undefined paths
 app.get('*', (req, res) => {
   res.status(404).send('Not found');
+});
+
+// Start the Server
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
 });
 
 module.exports = app;
