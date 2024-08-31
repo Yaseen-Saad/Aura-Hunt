0005945e30e6ee57e6baf8dea9727d202c11e52e
Yaseen Saad Eldin <yaseensaadeldinmohamed@gmail.com>
2024-08-31 13:10:18 +0300

    Probably fixed the github push issue

--- api/firebase.js
+++ api/firebase.js
@@ -1,9 +1,24 @@
-// firebase.js
 const admin = require('firebase-admin');
-const serviceAccount = JSON.parse(process.env.FIREBASECONFIGS)
+
+const serviceAccountString = process.env.FIREBASECONFIGS;
+if (!serviceAccountString) {
+  throw new Error('FIREBASECONFIGS environment variable is not set');
+}
+
+let serviceAccount;
+try {
+  serviceAccount = JSON.parse(serviceAccountString);
+} catch (error) {
+  throw new Error('Error parsing FIREBASECONFIGS environment variable: ' + error.message);
+}
+
+if (!serviceAccount.project_id) {
+  throw new Error('Service account object must contain a string "project_id" property.');
+}
+
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    storageBucket: 'aura-hunt.appspot.com' // Replace with your Firebase Storage bucket
+  credential: admin.credential.cert(serviceAccount),
+  storageBucket: 'aura-hunt.appspot.com' // Replace with your Firebase Storage bucket
 });
 
 const db = admin.firestore();
